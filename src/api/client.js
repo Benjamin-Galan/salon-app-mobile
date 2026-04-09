43609ce8788857450eda14a7a04d3c69e155e4e1
Benja-Dev <begalan@outlook.es>
2026-04-09 02:51:11 -0600

    validando sesion y rol

--- src/api/client.js
+++ src/api/client.js
@@ -1,8 +1,10 @@
 import axios from "axios";
+import { Alert } from "react-native";
 import { useAuthStore } from "../store/authStore";
 import { useLogStore } from "../store/logStore";
 
 const apiUrl = process.env.EXPO_PUBLIC_API_URL || "http://72.61.5.146/api";
+//const apiUrl = "http://192.168.1.2:8000/api";
 
 export const api = axios.create({
     baseURL: apiUrl,
@@ -39,6 +41,18 @@ api.interceptors.response.use(
         };
 
         useLogStore.getState().addLog('error', 'API Error', logData);
+
+        // Si es 401 (No autenticado) o 403 (No tiene permisos/rol adecuado)
+        if (error.response?.status === 401 || error.response?.status === 403) {
+
+            // Si es 403, avisar específicamente
+            if (error.response?.status === 403) {
+                Alert.alert("Acceso Denegado", "Tu perfil no tiene permisos de empleado o administrador para usar esta aplicación.");
+            }
+
+            // Limpiamos la sesión e inmediatamente Expo Router nos echará al login
+            useAuthStore.getState().logout();
+        }
 
         return Promise.reject(error);
     }
