e61b9c5cc155fdbaea2a012fcbcbc169c5dc3c4e
Benja-Dev <begalan@outlook.es>
2026-04-09 02:52:22 -0600

    implementando hidratacion

--- src/store/authStore.js
+++ src/store/authStore.js
@@ -4,29 +4,33 @@ import { createJSONStorage, persist } from 'zustand/middleware';
 
 export const useAuthStore = create(
     persist(
-        (set) => (
-            {
+        (set) => ({
+            user: null,
+            token: null,
+            isAuthenticated: false,
+            _hasHydrated: false, // <--- Nueva bandera
+
+            setHasHydrated: (state) => set({ _hasHydrated: state }),
+
+            login: (user, token) => set({
+                user,
+                token,
+                isAuthenticated: true
+            }),
+
+            logout: () => set({
                 user: null,
                 token: null,
-                isAuthenticated: false,
-
-                login: (user, token) => set({
-                    user,
-                    token,
-                    isAuthenticated: true
-                }),
-
-                logout: () => set({
-                    user: null,
-                    token: null,
-                    isAuthenticated: false
-                }),
-                
-            }
-        ),
+                isAuthenticated: false
+            }),
+        }),
         {
             name: "auth-storage",
-            storage: createJSONStorage(() => AsyncStorage )
+            storage: createJSONStorage(() => AsyncStorage),
+            // Se llama cuando zustand ya ha recuperado los datos de AsyncStorage
+            onRehydrateStorage: () => (state) => {
+                state?.setHasHydrated(true);
+            }
         }
     )
 )
